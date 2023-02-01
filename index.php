54d7398f8436f865e1a034d6eb1a0db2c0369100
Filipe Campos <116318582+wantedxp@users.noreply.github.com>
2023-02-01 02:48:35 -0300

    projeto formulario usando php e mysql.

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    
+<?php 
+    if(!isset($_POST['acao'])){
+        $email = strip_tags($_POST['email']);
+        $password = $_POST['password'];
+
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false){
+            echo 'Email invalido';
+        }else{
+            //passamos pela validação
+            $pdo = new PDO('mysql:host=localhost;dbname=formulario','root','');
+
+            $sql = $pdo->prepare("INSERT INTO login VALUES (null,?,?)");
+            $sql->execute(array($email,$password));
+
+            echo 'Inserido com sucesso';
+        }
+    }
+?>
+
+<div class="px-4 pt-5 my-5 text-center border-bottom">
+    <h4 class="display-4 fw-bold">Sign Up Form</h4>
+    
+<div class="container col-xl-12 col-xxl-12 px-4 py-5">
+    
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-12 border rounded-3 bg-light" method="post"> 
+          <div class="form-floating mb-3">
+            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <label for="floatingPassword">Password</label>
+          </div>
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Remember me
+            </label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+          <hr class="my-4">
+          <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
